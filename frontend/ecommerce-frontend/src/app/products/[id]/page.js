e810c6249df8c7f0b778a6aea230b4651697af6a
Volodymyr <volodymyr.pechenevskyi@gmail.com>
2025-02-11 19:16:27 +0100

    update product page

--- frontend/ecommerce-frontend/src/app/products/[id]/page.js
+++ frontend/ecommerce-frontend/src/app/products/[id]/page.js
@@ -31,15 +31,16 @@ export default function Product({ params }) {
 
     return (
         <div className="m-20 font-lato">
-            <Link href="/products/">Go back</Link>
+            <Link href="/products/"><p className="mb-5"><span className="mr-2">&#8617;</span>Go back</p></Link>
             <div className="flex gap-20">
                 <img className="max-w-[420px]" src={product.imageUrl} alt={product.description}></img>
                 <div>
                     <h1 className="text-4xl font-[600]">{product.name}</h1>
-                    <p className="text-2xl font-[400] text-[--secondary-text-color]">{product.description}</p>
-                    <p className="text-xl">Color: {product.color}</p>
+                    <p className="text-2xl font-[400] text-[--secondary-text-color] mt-5">{product.description}</p>
+                    <p className="text-xl mt-5">Color: {product.color}</p>
                     <p className="text-xl">Size: {product.size}</p>
-                    <p className="text-2xl font-[300]">{product.price} USD</p>
+                    <p className="text-2xl font-[300] mt-5">{product.price} USD</p>
+                    <button className="btn mt-10">Add to a cart</button>
                 </div>
             </div>
         </div>
